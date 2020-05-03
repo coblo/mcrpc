c6a31942df8b0dde6b3d6a1f9a7d2dec403bccba
Titusz Pan <titusz.pan@gmail.com>
2020-05-03 14:48:36 +0200

    Switch packaging to poetry

--- mcrpc/__init__.py
+++ mcrpc/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-
-"""Top-level package for mcrpc."""
-
-__author__ = """Titusz Pan"""
-__email__ = 'tp@py7.de'
-__version__ = '0.1.0'
+__version__ = '2.0.6.0'
 
 from mcrpc.client import RpcClient
