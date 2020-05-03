565cef9400ee666bca219f84332898e1ff96fdb6
Titusz Pan <titusz.pan@gmail.com>
2020-05-03 15:07:38 +0200

    Switch tests to pytest

--- tests/test_mcrpc.py
+++ tests/test_mcrpc.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Tests for `mcrpc` package."""
-import unittest
+import pytest
 from string import Template
 from decimal import Decimal
 from codegen import get_api_methods, get_stub_template
-from mcrpc import RpcClient
 from mcrpc.exceptions import RpcError
 from mcrpc import responses
 
 
-class TestMCRPC(unittest.TestCase):
-    """Tests for `mcrpc` package."""
+@pytest.fixture
+def client():
+    import mcrpc
+    return mcrpc.autoconnect()
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-        self.c = RpcClient(
-            '127.0.0.1', '9000', 'testuser', 'testpassword', False
-        )
 
-    def test_getinfo(self):
-        r = self.c.getinfo()
-        self.assertIsInstance(r, responses.Getinfo)
+def test_getinfo(client):
+    r = client.getinfo()
+    assert isinstance(r, responses.Getinfo)
 
-    def test_getbalance(self):
-        r = self.c.getbalance()
-        self.assertIsInstance(r, Decimal)
 
-    def test_nonexisting_method(self):
-        with self.assertRaises(RpcError):
-            self.c.noexist()
+def test_getbalance(client):
+    r = client.getbalance()
+    assert isinstance(r, Decimal)
 
-    def test_load_stub_template(self):
-        self.assertIsInstance(get_stub_template(), Template)
 
-    def test_get_api_methods(self):
-        methods = get_api_methods(self.c)
-        self.assertEqual(len(methods), 170)
-        self.c.getinfo()
+def test_nonexisting_method(client):
+    with pytest.raises(RpcError):
+        client.noexist()
 
-    def test_encoding_text(self):
-        s = 'I sat döwn for “coffee” at the café'
-        try:
-            self.c.create('stream', 'test', True)
-        except RpcError:
-            pass
-        txid = self.c.publish('test', 'testkey', {'text': s})
-        resp = self.c.getstreamitem('test', txid)
-        rs = resp['data']['text']
-        self.assertEqual(rs, s)
+
+def test_load_stub_template(client):
+    assert isinstance(get_stub_template(), Template)
+
+
+def test_get_api_methods(client):
+    methods = get_api_methods(client)
+    assert len(methods) == 170
+
+
+def test_encoding_text(client):
+    s = 'I sat döwn for “coffee” at the café'
+    try:
+        client.create('stream', 'test', True)
+    except RpcError:
+        pass
+    txid = client.publish('test', 'testkey', {'text': s})
+    resp = client.getstreamitem('test', txid)
+    rs = resp['data']['text']
+    assert rs == s
+
